9c0be416223a1f21110b0f90a425ab3f2ca6ca77
wangxg2016 <wang_xg@139.com>
2016-10-18 23:20:07 +0800

    about

--- src/demos/About.vue
+++ src/demos/About.vue
@@ -1,19 +1,10 @@
 <template>
-<xcontent>
+<xcontent :classname="'bg-light'">
   <span slot="title">关于...</span>
   <div class="box no-shadow">
       <p class="title">Vue-Bulma</p>
       <p class="subtitle">轻量级高性能MVVM Admin UI框架</p>
-      <ul>
-        <li>MVVM 数据驱动</li>
-        <li>前后端分离</li>
-        <li>组件化设计</li>
-        <li>flex布局</li>
-        <li>offline support</li>
-        <li>ajax cache</li>
-        <li>without jQuery/zepto</li>
-        <li><strong>Upgrade to Vue 2.0</strong></li>
-      </ul>
+      <p><span class="icon"><i class="fa fa-github"></i></span><a src="https://github.com/wangxg2016/vue-bulma">https://github.com/wangxg2016/vue-bulma</a></p>
   </div>
 </xcontent>
 </template>
