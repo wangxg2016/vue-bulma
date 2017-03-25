43566dbd8f20e34c0955c05321e8a2253631cd3d
yoqu <yoqulin@qq.com>
2017-03-25 22:22:02 +0800

    fix html 结尾未添加结尾符启动 webpack 报错的 bug

--- src/App.vue
+++ src/App.vue
@@ -14,230 +14,263 @@
     <login v-if='!isAuth' class="is-active"></login>
     <toast :toast-array="toastArray"></toast>
     <transition name="fade">
-    <modal v-show="isModal" @closemodal="closeModal()" :mode="modalinfo.mode" :title="modalinfo.title"" :content="modalinfo.content" :width="modalinfo.width" :ok="modalinfo.ok"" :cancel="modalinfo.cancel"></modal>
+      <modal v-show="isModal" @closemodal="closeModal()" :mode="modalinfo.mode" :title="modalinfo.title"
+      " :content="modalinfo.content" :width="modalinfo.width" :ok="modalinfo.ok"" :cancel="modalinfo.cancel"></modal>
     </transition>
-  </div>
+    </div>
+  </section>
 </template>
 <script>
-// vuex全局数据
-import store from './store'
-import Xheader from './components/header'
-import { Toast, Modal } from './components/modal'
-import { NavMenu, Login } from './components/page'
-export default {
-  store: store,
-  components: {
-    NavMenu,
-    Login,
-    Modal,
-    Toast,
-    Xheader
-  },
-  data () {
-    return {
-      modalinfo: {
-        mode: 'alert',
-        title: '',
-        content: '',
-        callback: null
+  // vuex全局数据
+  import store from './store'
+  import Xheader from './components/header'
+  import {Toast, Modal} from './components/modal'
+  import {NavMenu, Login} from './components/page'
+  export default {
+    store: store,
+    components: {
+      NavMenu,
+      Login,
+      Modal,
+      Toast,
+      Xheader
+    },
+    data () {
+      return {
+        modalinfo: {
+          mode: 'alert',
+          title: '',
+          content: '',
+          callback: null
+        }
+        // data
       }
-      // data
-    }
-  },
-  computed: {
-    isModal () {
-      return this.$store.getters.getModal
     },
-    isAuth () {
-      return this.$store.getters.getIsAuth
-    },
-    toastArray () {
-      return this.$store.getters.getToast
-    },
-    preloaderShow () {
-      return this.$store.getters.getPreloader
-    }
-  },
-  methods: {
-    showModal (mode, title, content, width, ok, cancel) {
-      this.$set(this.modalinfo, 'mode', mode)
-      this.$set(this.modalinfo, 'title', title)
-      this.$set(this.modalinfo, 'content', content)
-      this.$set(this.modalinfo, 'width', width)
-      this.$set(this.modalinfo, 'ok', ok)
-      this.$set(this.modalinfo, 'cancel', cancel)
-      store.commit('modal', true)
-    },
-    closeModal () {
-      store.commit('modal', false)
-    }
-  },
-  mounted () {
-    let ico = require('./assets/images/vueico.png')
-    let icon = require('./assets/images/vuelogo.png')
-
-    document.getElementById('linkIcon').href = ico
-    document.getElementById('linkAppIcon').href = icon
-
-    window.applicationCache.addEventListener('updateready', (e) => {
-      if (window.applicationCache.status === window.applicationCache.UPDATEREADY) {
-        console.log('system update')
-        window.location.reload()
+    computed: {
+      isModal () {
+        return this.$store.getters.getModal
+      },
+      isAuth () {
+        return this.$store.getters.getIsAuth
+      },
+      toastArray () {
+        return this.$store.getters.getToast
+      },
+      preloaderShow () {
+        return this.$store.getters.getPreloader
       }
-    }, false)
+    },
+    methods: {
+      showModal (mode, title, content, width, ok, cancel) {
+        this.$set(this.modalinfo, 'mode', mode)
+        this.$set(this.modalinfo, 'title', title)
+        this.$set(this.modalinfo, 'content', content)
+        this.$set(this.modalinfo, 'width', width)
+        this.$set(this.modalinfo, 'ok', ok)
+        this.$set(this.modalinfo, 'cancel', cancel)
+        store.commit('modal', true)
+      },
+      closeModal () {
+        store.commit('modal', false)
+      }
+    },
+    mounted () {
+      let ico = require('./assets/images/vueico.png')
+      let icon = require('./assets/images/vuelogo.png')
+
+      document.getElementById('linkIcon').href = ico
+      document.getElementById('linkAppIcon').href = icon
+
+      window.applicationCache.addEventListener('updateready', (e) => {
+        if (window.applicationCache.status === window.applicationCache.UPDATEREADY) {
+          console.log('system update')
+          window.location.reload()
+        }
+      }, false)
+    }
   }
-}
 </script>
 <style lang="less">
-@media screen and (min-width: 769px){
-.modal-content, .modal-card {
-  margin:inherit;
-}
-}
-.tabs a{
-  transition: all 100ms;
-}
+  @media screen and (min-width: 769px) {
+    .modal-content, .modal-card {
+      margin: inherit;
+    }
+  }
 
-html, body{
-  background-color:#fff;
-  font-family: Helvetica Neue,Helvetica,"Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
-  overflow:hidden;
-}
-#app-container{
-  position:absolute;
-  left:0;
-  right:0;
-  top:0;
-  bottom:0;
-  overflow:hidden;  
-}
-section.page{
-  position:absolute;
-  top:50px;
-  left:0;
-  right:0;
-  bottom:0;
-  overflow:hidden;
-  margin:0;
-}
-section.page-content,.fade{
-  opacity: 1;
-  transition: opacity 300ms;
-}
-.fade-enter, .fade-leave-active {
-  opacity: 0 !important;
-}
+  .tabs a {
+    transition: all 100ms;
+  }
 
-.slide{
-  transform: translate3d(0,0,0);
-  transition: all 400ms;
-}
-.slide-enter, .slide-leave-active {
-  transform: translate3d(-110%,0,0);
-}
-.transiton-reverse .slide-enter, .transiton-reverse  .slide-leave-active {
-  transform: translate3d(110%,0,0);
-}
-a .icon {
+  html, body {
+    background-color: #fff;
+    font-family: Helvetica Neue, Helvetica, "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+    overflow: hidden;
+  }
+
+  #app-container {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    overflow: hidden;
+  }
+
+  section.page {
+    position: absolute;
+    top: 50px;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    overflow: hidden;
+    margin: 0;
+  }
+
+  section.page-content, .fade {
+    opacity: 1;
+    transition: opacity 300ms;
+  }
+
+  .fade-enter, .fade-leave-active {
+    opacity: 0 !important;
+  }
+
+  .slide {
+    transform: translate3d(0, 0, 0);
+    transition: all 400ms;
+  }
+
+  .slide-enter, .slide-leave-active {
+    transform: translate3d(-110%, 0, 0);
+  }
+
+  .transiton-reverse .slide-enter, .transiton-reverse .slide-leave-active {
+    transform: translate3d(110%, 0, 0);
+  }
+
+  a .icon {
     vertical-align: baseline;
-    margin-right:5px;
-}
-.is-rounded{
-  border-radius: 4px;
-}
-.is-right-5{
-  margin-right:5px;
-}
-.is-right-10{
-  margin-right:10px;
-}
-.is-right-20{
-  margin-right:20px;
-}
-.is-left-10{
-  margin-left:10px;
-}
-.is-bottom-5{
-  margin-bottom:5px !important;
-}
-.is-bottom-10{
-  margin-bottom:10px;
-}
-.is-bottom-20{
-  margin-bottom:20px;
-}
-.is-circle{
-  border-radius: 50%;
-}
-.is-baseline{
-  vertical-align: baseline;
-}
-.is-middle{
-  vertical-align: middle;
-}
-.is-flex{
-  display:flex;
-}
-.no-shadow{
-  box-shadow:none;
-}
-.has-shadow{
-  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);
-}
-.notification.is-normal {
+    margin-right: 5px;
+  }
+
+  .is-rounded {
+    border-radius: 4px;
+  }
+
+  .is-right-5 {
+    margin-right: 5px;
+  }
+
+  .is-right-10 {
+    margin-right: 10px;
+  }
+
+  .is-right-20 {
+    margin-right: 20px;
+  }
+
+  .is-left-10 {
+    margin-left: 10px;
+  }
+
+  .is-bottom-5 {
+    margin-bottom: 5px !important;
+  }
+
+  .is-bottom-10 {
+    margin-bottom: 10px;
+  }
+
+  .is-bottom-20 {
+    margin-bottom: 20px;
+  }
+
+  .is-circle {
+    border-radius: 50%;
+  }
+
+  .is-baseline {
+    vertical-align: baseline;
+  }
+
+  .is-middle {
+    vertical-align: middle;
+  }
+
+  .is-flex {
+    display: flex;
+  }
+
+  .no-shadow {
+    box-shadow: none;
+  }
+
+  .has-shadow {
+    box-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);
+  }
+
+  .notification.is-normal {
     background-color: #fff;
     color: rgba(0, 0, 0, 0.7);
-}
-#nav-section{
-    background-color: rgb(0,151,129) !important;
+  }
+
+  #nav-section {
+    background-color: rgb(0, 151, 129) !important;
     position: fixed;
     right: 0;
     left: 0;
     z-index: 1030;
-}
-#nav-section .nav{
-  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);
-}
-section.page>.columns{
-  margin:0;
-}
-.page-container{
-  position:absolute;
-  left:0;
-  top:0;
-  right:0;
-  bottom:0;
-  overflow:hidden;
-  background-color: #fff;
-}
-.page-content{
-  position:absolute;
-  left:220px;
-  top:0;
-  right:0;
-  bottom:0;
-  overflow:auto;
-  transition: all 0.3s;
-}
-.inner-header, .inner-content{
-  padding: 20px;
-}
+  }
 
+  #nav-section .nav {
+    box-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);
+  }
 
-.example:not(:last-child), .structure:not(:last-child) {
+  section.page > .columns {
+    margin: 0;
+  }
+
+  .page-container {
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    overflow: hidden;
+    background-color: #fff;
+  }
+
+  .page-content {
+    position: absolute;
+    left: 220px;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    overflow: auto;
+    transition: all 0.3s;
+  }
+
+  .inner-header, .inner-content {
+    padding: 20px;
+  }
+
+  .example:not(:last-child), .structure:not(:last-child) {
     margin-bottom: 20px;
-}
-.example:not(:first-child), .structure:not(:first-child) {
+  }
+
+  .example:not(:first-child), .structure:not(:first-child) {
     margin-top: 30px;
-}
-.example, .structure {
+  }
+
+  .example, .structure {
     border: 1px solid #ffdd57;
     border-top-right-radius: 3px;
     color: rgba(0, 0, 0, 0.7);
     padding: 15px 20px;
     position: relative;
-}
-.example:before, .structure:before {
+  }
+
+  .example:before, .structure:before {
     background: #ffdd57;
     border-radius: 3px 3px 0 0;
     bottom: 100%;
@@ -251,11 +284,13 @@ section.page>.columns{
     position: absolute;
     text-transform: uppercase;
     vertical-align: top;
-}
-.highlight {
+  }
+
+  .highlight {
     position: relative;
-}
-.highlight .copy, .highlight .expand {
+  }
+
+  .highlight .copy, .highlight .expand {
     -webkit-touch-callout: none;
     -webkit-user-select: none;
     -moz-user-select: none;
@@ -270,20 +305,24 @@ section.page>.columns{
     position: absolute;
     right: 0;
     top: 0;
-}
-.example + .highlight:not(:last-child) {
+  }
+
+  .example + .highlight:not(:last-child) {
     margin-bottom: 40px;
-}
-.example + .highlight {
+  }
+
+  .example + .highlight {
     border: 1px solid #ffdd57;
     border-radius: 0 0 3px 3px;
     border-top: none;
     margin-top: -20px;
-}
-.bg-light{
-  background-color:#f1f1f1;
-}
-button.copy{
-  display:none;
-}
+  }
+
+  .bg-light {
+    background-color: #f1f1f1;
+  }
+
+  button.copy {
+    display: none;
+  }
 </style>
--- src/demos/Index.vue
+++ src/demos/Index.vue
@@ -1,156 +1,161 @@
 <template>
-<xcontent :classname="'bg-light'">
-  <span slot="title"><span class="icon is-middle"><i class="fa fa-home is-right-5"></i></span>首页</span>
-  <div slot="menu" class="tabs is-boxed" style="margin:10px 20px 0;">
-    <ul class="is-left">
-      <li :class="{'is-active':index===0}" @click="change(0)"><a>简介</a></li>
-      <li :class="{'is-active':index===1}" @click="change(1)"><a>布局</a></li>
-      <li :class="{'is-active':index===2}" @click="change(2)"><a>全局</a></li>
-      <li :class="{'is-active':index===3}" @click="change(3)"><a>缓存</a></li>
-    </ul>
-  </div>
-<div class="tabs-content-container">
-<div class="fade" v-show="index===0">
-<div class="tile is-ancestor">
-  <div class="tile is-parent">
-    <article class="tile is-child notification is-normal">
-      <div class="content">
-        <p class="title">Vue-Bulma</p>
-        <p class="subtitle">轻量级高性能MVVM Admin UI框架</p>
-        <p>偶然发现了bulma，自然语义定义的方式让人耳目一新，特别gzip后13.6k的size令人印象深刻，刚好基于vue+weui的移动端组件库demo已经接近完成，于是便抽空做了这套基于vue+bulma的轻量级UI。既然是后台管理UI，便可以任性的对IE无视，此处应有掌声。</p>
-        <p>为简化部署及升级便利，UI处理上原则不变动bulma的基础设置，只增加差异化的部分，同时谈化各种切换效果，力求整个UI界面的小清新。</p>
-        <div class="content">
-          <!-- Content -->
+  <xcontent :classname="'bg-light'">
+    <span slot="title"><span class="icon is-middle"><i class="fa fa-home is-right-5"></i></span>首页</span>
+    <div slot="menu" class="tabs is-boxed" style="margin:10px 20px 0;">
+      <ul class="is-left">
+        <li :class="{'is-active':index===0}" @click="change(0)"><a>简介</a></li>
+        <li :class="{'is-active':index===1}" @click="change(1)"><a>布局</a></li>
+        <li :class="{'is-active':index===2}" @click="change(2)"><a>全局</a></li>
+        <li :class="{'is-active':index===3}" @click="change(3)"><a>缓存</a></li>
+      </ul>
+    </div>
+    <div class="tabs-content-container">
+      <div class="fade" v-show="index===0">
+        <div class="tile is-ancestor">
+          <div class="tile is-parent">
+            <article class="tile is-child notification is-normal">
+              <div class="content">
+                <p class="title">Vue-Bulma</p>
+                <p class="subtitle">轻量级高性能MVVM Admin UI框架</p>
+                <p>
+                  偶然发现了bulma，自然语义定义的方式让人耳目一新，特别gzip后13.6k的size令人印象深刻，刚好基于vue+weui的移动端组件库demo已经接近完成，于是便抽空做了这套基于vue+bulma的轻量级UI。既然是后台管理UI，便可以任性的对IE无视，此处应有掌声。</p>
+                <p>为简化部署及升级便利，UI处理上原则不变动bulma的基础设置，只增加差异化的部分，同时谈化各种切换效果，力求整个UI界面的小清新。</p>
+                <div class="content">
+                  <!-- Content -->
+                </div>
+              </div>
+            </article>
+          </div>
         </div>
       </div>
-    </article>
-  </div>
-</div>
-</div>
 
-<div class="fade" v-show="index===1">
-<div class="tile is-ancestor">
-  <div class="tile is-parent">
-    <article class="tile is-child notification is-normal">
-      <div class="content">
-        <p class="title">Flex Layout</p>
-        <p class="subtitle">弹性盒内滚动布局</p>
-        <p>支持所有新一代浏览器，为获得最优的效果优先推荐使用最新版Chrome，本项目设计为PC端的管理后台使用。</p>
-        <div class="content">
-          <!-- Content -->
+      <div class="fade" v-show="index===1">
+        <div class="tile is-ancestor">
+          <div class="tile is-parent">
+            <article class="tile is-child notification is-normal">
+              <div class="content">
+                <p class="title">Flex Layout</p>
+                <p class="subtitle">弹性盒内滚动布局</p>
+                <p>支持所有新一代浏览器，为获得最优的效果优先推荐使用最新版Chrome，本项目设计为PC端的管理后台使用。</p>
+                <div class="content">
+                  <!-- Content -->
+                </div>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div class="tile is-ancestor">
+          <div class="tile is-vertical is-8">
+            <div class="tile">
+              <div class="tile is-parent is-vertical">
+                <article class="tile is-child notification is-primary">
+                  <p class="title">Vertical...</p>
+                  <p class="subtitle">Top tile</p>
+                </article>
+                <article class="tile is-child notification is-warning">
+                  <p class="title">...tiles</p>
+                  <p class="subtitle">Bottom tile</p>
+                </article>
+              </div>
+              <div class="tile is-parent">
+                <article class="tile is-child notification is-info">
+                  <p class="title">Middle tile</p>
+                  <p class="subtitle">With an image</p>
+                  <figure class="image is-4by3">
+                    <img src="http://placehold.it/640x480">
+                  </figure>
+                </article>
+              </div>
+            </div>
+            <div class="tile is-parent">
+              <article class="tile is-child notification is-danger">
+                <p class="title">Wide tile</p>
+                <p class="subtitle">Aligned with the right tile</p>
+                <div class="content">
+                  <!-- Content -->
+                </div>
+              </article>
+            </div>
+          </div>
+          <div class="tile is-parent">
+            <article class="tile is-child notification is-success">
+              <div class="content">
+                <p class="title">Tall tile</p>
+                <p class="subtitle">With even more content</p>
+                <div class="content">
+                  <!-- Content -->
+                </div>
+              </div>
+            </article>
+          </div>
         </div>
       </div>
-    </article>
-  </div>
-</div>
-<div class="tile is-ancestor">
-  <div class="tile is-vertical is-8">
-    <div class="tile">
-      <div class="tile is-parent is-vertical">
-        <article class="tile is-child notification is-primary">
-          <p class="title">Vertical...</p>
-          <p class="subtitle">Top tile</p>
-        </article>
-        <article class="tile is-child notification is-warning">
-          <p class="title">...tiles</p>
-          <p class="subtitle">Bottom tile</p>
-        </article>
+
+      <div class="fade" v-show="index===2">
+        <div class="tile is-ancestor">
+          <div class="tile is-parent">
+            <article class="tile is-child notification is-normal">
+              <div class="content">
+                <p class="title">全局组件</p>
+                <p class="subtitle">Modal、Toast、Preloader</p>
+                <p>为了更方便的调用经常性组件，特别设计为全局组件，可处理多数场景，复杂的处理请自行添加入页面级组件</p>
+                <div class="content">
+                  <!-- Content -->
+                </div>
+              </div>
+            </article>
+          </div>
+        </div>
       </div>
-      <div class="tile is-parent">
-        <article class="tile is-child notification is-info">
-          <p class="title">Middle tile</p>
-          <p class="subtitle">With an image</p>
-          <figure class="image is-4by3">
-            <img src="http://placehold.it/640x480">
-          </figure>
-        </article>
+
+      <div class="fade" v-show="index===3">
+        <div class="tile is-ancestor">
+          <div class="tile is-parent">
+            <article class="tile is-child notification is-normal">
+              <div class="content">
+                <p class="title">缓存</p>
+                <p class="subtitle">keep-alive dom cache、ajax data cache、manifest resource cache</p>
+                <p>
+                  想让系统更加顺畅，缓存必不可少，vue-bulma中除了vue内置的keep-alive对dom进行缓存外，同时具备ajax的数据缓存机制。另外，还准备了manifest缓存指引，让前端飞起来。。。</p>
+                <div class="content">
+                  <!-- Content -->
+                </div>
+              </div>
+            </article>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="tile is-parent">
-      <article class="tile is-child notification is-danger">
-        <p class="title">Wide tile</p>
-        <p class="subtitle">Aligned with the right tile</p>
-        <div class="content">
-          <!-- Content -->
-        </div>
-      </article>
-    </div>
-  </div>
-  <div class="tile is-parent">
-    <article class="tile is-child notification is-success">
-      <div class="content">
-        <p class="title">Tall tile</p>
-        <p class="subtitle">With even more content</p>
-        <div class="content">
-          <!-- Content -->
-        </div>
-      </div>
-    </article>
-  </div>
-</div>
-</div>
-
-<div class="fade" v-show="index===2">
-<div class="tile is-ancestor">
-  <div class="tile is-parent">
-    <article class="tile is-child notification is-normal">
-      <div class="content">
-        <p class="title">全局组件</p>
-        <p class="subtitle">Modal、Toast、Preloader</p>
-        <p>为了更方便的调用经常性组件，特别设计为全局组件，可处理多数场景，复杂的处理请自行添加入页面级组件</p>
-        <div class="content">
-          <!-- Content -->
-        </div>
-      </div>
-    </article>
-  </div>
-</div>
-</div>
-
-<div class="fade" v-show="index===3">
-<div class="tile is-ancestor">
-  <div class="tile is-parent">
-    <article class="tile is-child notification is-normal">
-      <div class="content">
-        <p class="title">缓存</p>
-        <p class="subtitle">keep-alive dom cache、ajax data cache、manifest resource cache</p>
-        <p>想让系统更加顺畅，缓存必不可少，vue-bulma中除了vue内置的keep-alive对dom进行缓存外，同时具备ajax的数据缓存机制。另外，还准备了manifest缓存指引，让前端飞起来。。。</p>
-        <div class="content">
-          <!-- Content -->
-        </div>
-      </div>
-    </article>
-  </div>
-</div>
-</div>
-
-</xcontent>
+  </xcontent>
 </template>
 <script>
-import { Xcontent } from '../components/page'
-export default {
-  components: {
-    Xcontent
-  },
-  data () {
-    return {
-      index: 0
-    }
-  },
-  methods: {
-    change (index) {
-      if (this.index === index) return
-      this.$nextTick(() => { this.index = index })
+  import {Xcontent} from '../components/page'
+  export default {
+    components: {
+      Xcontent
+    },
+    data () {
+      return {
+        index: 0
+      }
+    },
+    methods: {
+      change (index) {
+        if (this.index === index) return
+        this.$nextTick(() => {
+          this.index = index
+        })
+      }
     }
   }
-}
 </script>
 <style lang="less" scoped>
- .tabs-content-container .fade-leave-active {
-    display:none;
- }
- .tabs-content-container .fade-enter {
+  .tabs-content-container .fade-leave-active {
+    display: none;
+  }
+
+  .tabs-content-container .fade-enter {
     opacity: 0.5 !important;
- }
+  }
 </style>
 
